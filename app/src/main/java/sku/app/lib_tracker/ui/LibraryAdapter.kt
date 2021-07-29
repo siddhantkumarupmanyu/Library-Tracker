1f919fdd5e1883885299776fe7c720161f600e47
Siddhant Kumar <siddhantkumarupmanyu@gmail.com>
2021-07-30 00:21:15 +0530

    remove letter spacing
    since letter spacing does not directly map to right em. We are left with using the pre-calculated ones.

--- app/src/main/java/sku/app/lib_tracker/ui/LibraryAdapter.kt
+++ app/src/main/java/sku/app/lib_tracker/ui/LibraryAdapter.kt
@@ -1,15 +1,12 @@
 package sku.app.lib_tracker.ui
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import sku.app.lib_tracker.databinding.ArtifactItemBinding
 import sku.app.lib_tracker.databinding.ListItemBinding
 import sku.app.lib_tracker.vo.Library
-import kotlin.math.min
 
 class LibraryAdapter : ListAdapter<Library, RecyclerView.ViewHolder>(DiffCallback()) {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
@@ -31,8 +28,12 @@ class LibraryAdapter : ListAdapter<Library, RecyclerView.ViewHolder>(DiffCallbac
         RecyclerView.ViewHolder(binding.root) {
 
         init {
+            binding.cardView.setOnLongClickListener {
+                binding.cardView.isChecked = !binding.cardView.isChecked
+                true
+            }
             // data-binding hack
-            binding.linearLayout.removeViewAt(0)
+            // binding.linearLayout.removeViewAt(0)
         }
 
         fun bind(item: Library) {
@@ -45,49 +46,49 @@ class LibraryAdapter : ListAdapter<Library, RecyclerView.ViewHolder>(DiffCallbac
         }
 
         private fun addArtifacts(item: Library) {
-            val childCount = binding.linearLayout.childCount
-            val artifactsSize = item.artifacts.size
-
-            val minSize = min(childCount, artifactsSize)
-
-            var i = 0
-
-            // populate views
-            while (i < minSize) {
-                val view = binding.linearLayout.getChildAt(i)
-                view.visibility = View.VISIBLE
-
-                val binding = view.tag as ArtifactItemBinding
-                binding.artifact = item.artifacts[i]
-                binding.executePendingBindings()
-
-                i++
-            }
-
-            // create new views
-            while (i < artifactsSize) {
-                val artifactBinding = ArtifactItemBinding.inflate(
-                    LayoutInflater.from(binding.linearLayout.context),
-                    binding.linearLayout,
-                    false
-                )
-                binding.linearLayout.addView(artifactBinding.root)
-
-                artifactBinding.root.tag = artifactBinding
-
-                artifactBinding.artifact = item.artifacts[i]
-                artifactBinding.executePendingBindings()
-
-                i++
-            }
-
-            // hide remaining views if any
-            while (i < childCount) {
-                val view = binding.linearLayout.getChildAt(i)
-                view.visibility = View.GONE
-
-                i++
-            }
+            // val childCount = binding.linearLayout.childCount
+            // val artifactsSize = item.artifacts.size
+            //
+            // val minSize = min(childCount, artifactsSize)
+            //
+            // var i = 0
+            //
+            // // populate views
+            // while (i < minSize) {
+            //     val view = binding.linearLayout.getChildAt(i)
+            //     view.visibility = View.VISIBLE
+            //
+            //     val binding = view.tag as ArtifactItemBinding
+            //     binding.artifact = item.artifacts[i]
+            //     binding.executePendingBindings()
+            //
+            //     i++
+            // }
+            //
+            // // create new views
+            // while (i < artifactsSize) {
+            //     val artifactBinding = ArtifactItemBinding.inflate(
+            //         LayoutInflater.from(binding.linearLayout.context),
+            //         binding.linearLayout,
+            //         false
+            //     )
+            //     binding.linearLayout.addView(artifactBinding.root)
+            //
+            //     artifactBinding.root.tag = artifactBinding
+            //
+            //     artifactBinding.artifact = item.artifacts[i]
+            //     artifactBinding.executePendingBindings()
+            //
+            //     i++
+            // }
+            //
+            // // hide remaining views if any
+            // while (i < childCount) {
+            //     val view = binding.linearLayout.getChildAt(i)
+            //     view.visibility = View.GONE
+            //
+            //     i++
+            // }
 
         }
     }
